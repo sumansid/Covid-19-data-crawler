f59f3b6e2b147c1cbe837a6ee8c5c630544f1851
sumansid <53033648+sumansid@users.noreply.github.com>
2020-03-17 14:14:14 -0400

    Add files via upload

--- covid19app.py
+++ covid19app.py
@@ -9,35 +9,49 @@ import requests
 from selenium import webdriver
 
 driver = webdriver.Chrome('/Users/sumansigdel/Documents/chromedriver')
+res = driver.get("https://coronavirus.app/")
 
 def scrape_country_name():
-    res = driver.get("https://coronavirus.app/")
     #res = requests.get(url)
     html = driver.execute_script("return document.documentElement.outerHTML;")
-
     soup = BeautifulSoup(html, "lxml")
     #box = soup.findAll("div",{"class": "map-sidebar-section-content"})
-
     box = soup.find_all("div",{"class":"map-sidebar-section-item-title"})
-    driver.quit()
     return(box)
 
 #print(scrape_country_name())
 
 def scrape_country_infectants() :
-    res = driver.get("https://coronavirus.app/")
+    #res = driver.get("https://coronavirus.app/")
     # res = requests.get(url)
     html = driver.execute_script("return document.documentElement.outerHTML;")
-
     soup = BeautifulSoup(html, "lxml")
     # box = soup.findAll("div",{"class": "map-sidebar-section-content"})
-
     box = soup.find_all("div", {"class": "map-sidebar-section-item-nb infected"})
-    driver.quit()
     return (box)
 
-returned_data = scrape_country_infectants()
-for row in returned_data:
-    print(row.text)
+
+def infectants_array():
+    returned_data = scrape_country_infectants()
+    infect_array = [row.text for row in returned_data]
+    return infect_array
+
+def countries_array():
+    country_returned_data = scrape_country_name()
+    return_array = [row.text for row in country_returned_data]
+    return return_array
+
+
+def array_to_dict():
+    dictionary = dict(zip(countries_array(), infectants_array()))
+    return dictionary
+
+
+
+
+
+
+
+
 
 
--- saveInExcel.py
+++ saveInExcel.py
@@ -4,3 +4,13 @@ Author : Suman Sigdel
 File : saveInExcel : Gets the data from covid19app.py file and saves the data in an excel file
 """
 
+import covid19app
+
+def save_in_excel():
+    dict_with_data = covid19app.array_to_dict()
+
+
+
+
+
+
