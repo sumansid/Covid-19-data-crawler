56f5a6fdce87052b0fd5a8e929fe842e1f09c1da
sumansid <53033648+sumansid@users.noreply.github.com>
2020-04-09 00:00:05 -0400

    Update saveInExcel.py

--- saveInExcel.py
+++ saveInExcel.py
@@ -7,14 +7,12 @@ File : saveInExcel : Gets the data from covid19app.py file and saves the data in
 import covid19app
 
 def save_in_excel():
+    """
+    Function that converts Dictionaries and writes them in a CSV file
+    """
     dict_with_data = covid19app.array_to_dict()
     with open('covid19_data.csv', 'w') as f:
         for key in dict_with_data.keys():
             f.write("%s,%s\n"%(key,dict_with_data[key]))
 
 save_in_excel()
-
-
-
-
-
