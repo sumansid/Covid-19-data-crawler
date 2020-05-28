5076190940ea0d4c2f2424815a865c77907a58e1
Suman Sigdel <53033648+sumansid@users.noreply.github.com>
2020-05-28 00:27:27 -0400

    Update covid19app.py

--- covid19app.py
+++ covid19app.py
@@ -1,35 +1,26 @@
 """
-Date : 15/04/2020
+Date : 25/05/2020
 Author : Suman Sigdel
-File : CoronaVirus.app : Scrapes the Covid-19 data from dynamic JS sites, ie. coronavirus.app
+File : CoronaVirus.app : Scrapes the Covid-19 data and puts in an excel file
 """
 
 from bs4 import BeautifulSoup
-import requests
+#import requests
 from selenium import webdriver
-# Path to the Chromedriver
-driver = webdriver.Chrome('/Users///chromedriver') 
-# Setting up the request
-res = driver.get("https://coronavirus.app/") 
+driver = webdriver.Chrome("/Users/sumansigdel/Desktop/chromedriver")
+res = driver.get("https://coronavirus.app/")
+html = driver.execute_script("return document.documentElement.outerHTML;")
+soup = BeautifulSoup(html, "lxml")
 
 def scrape_country_name():
-    #res = requests.get(url)
-    html = driver.execute_script("return document.documentElement.outerHTML;")
-    soup = BeautifulSoup(html, "lxml")
-    #box = soup.findAll("div",{"class": "map-sidebar-section-content"})
     box = soup.find_all("div",{"class":"map-sidebar-section-item-title"})
-    return(box)
-
-#print(scrape_country_name())
+    print(box)
+    return box
 
 def scrape_country_infectants() :
-    #res = driver.get("https://coronavirus.app/")
-    # res = requests.get(url)
-    html = driver.execute_script("return document.documentElement.outerHTML;")
-    soup = BeautifulSoup(html, "lxml")
-    # box = soup.findAll("div",{"class": "map-sidebar-section-content"})
     box = soup.find_all("div", {"class": "map-sidebar-section-item-nb infected"})
-    return (box)
+    print(box)
+    return box
 
 
 def infectants_array():
@@ -41,7 +32,6 @@ def countries_array():
     country_returned_data = scrape_country_name()
     return_array = [row.text for row in country_returned_data]
     return return_array
-
 
 def array_to_dict():
     dictionary = dict(zip(countries_array(), infectants_array()))
